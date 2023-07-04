433134c0214b9026676041152665bbc76ff8b2b6
angie <74880133+debagiranje@users.noreply.github.com>
2023-07-04 09:21:22 +0200

    cs

---
+++ mrezni-plejer/src/application/Main.java
@@ -0,0 +1,162 @@
+package application;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Main extends Application {
+    private static final String SERVER_IP = " 192.168.56.1"; // Replace with the server IP address
+    private static final int SERVER_PORT = 6666;
+
+    private MediaPlayer mediaPlayer;
+    private Socket socket;
+
+    @Override
+    public void start(Stage primaryStage) {
+        VBox root = new VBox();
+        TextField songTextField = new TextField();
+        Button addButton = new Button("Add to Queue");
+        Button playButton = new Button("Play");
+        Button closeButton = new Button("Close Connection");
+        root.getChildren().addAll(songTextField, addButton, playButton, closeButton);
+
+        addButton.setOnAction(event -> {
+            String songName = songTextField.getText();
+            if (!songName.isEmpty()) {
+                addToQueue(songName);
+                songTextField.clear();
+            }
+        });
+
+        playButton.setOnAction(event -> {
+            if (mediaPlayer != null && mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
+                mediaPlayer.stop();
+            }
+            requestNextSongFromServer();
+        });
+
+        closeButton.setOnAction(event -> {
+            closeConnection();
+            Platform.exit();
+        });
+
+        Scene scene = new Scene(root, 200, 200);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Jukebox Client");
+        primaryStage.show();
+    }
+
+    private void addToQueue(String songName) {
+        new Thread(() -> {
+            try {
+                if (socket == null || socket.isClosed()) {
+                    socket = new Socket(SERVER_IP, SERVER_PORT);
+                    System.out.println("Connected to server to add to queue");
+                }
+
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataOutputStream.writeUTF(songName);
+                dataOutputStream.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    private void requestNextSongFromServer() {
+        new Thread(() -> {
+            try {
+                if (socket == null || socket.isClosed()) {
+                    socket = new Socket(SERVER_IP, SERVER_PORT);
+                    System.out.println("Connected to server for song request");
+                }
+
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataOutputStream.writeUTF("NEXT");
+                dataOutputStream.flush();
+
+                InputStream inputStream = socket.getInputStream();
+                DataInputStream dataInputStream = new DataInputStream(inputStream);
+
+                // Read the response from the server
+                String response = dataInputStream.readUTF();
+                if (response.startsWith("Song not found")) {
+                    System.out.println(response);
+                } else {
+                    // Play the received song data
+                    playSong(inputStream);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    private void playSong(InputStream inputStream) {
+        try {
+            DataInputStream dataInputStream = new DataInputStream(inputStream);
+
+            // Read the size of the song data
+            long songSize = dataInputStream.readLong();
+
+            byte[] buffer = new byte[8192];
+            int bytesRead;
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+            // Read the song data from the input stream
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                byteArrayOutputStream.write(buffer, 0, bytesRead);
+                if (byteArrayOutputStream.size() >= songSize) {
+                    break;
+                }
+            }
+
+            byte[] songData = byteArrayOutputStream.toByteArray();
+            byteArrayOutputStream.close();
+
+            // Create a temporary file from the received data
+            File tempFile = File.createTempFile("temp_song", ".mp3");
+            FileOutputStream fileOutputStream = new FileOutputStream(tempFile);
+            fileOutputStream.write(songData);
+            fileOutputStream.close();
+
+            // Play the temporary file
+            String tempFilePath = tempFile.toURI().toString();
+            Media media = new Media(tempFilePath);
+            mediaPlayer = new MediaPlayer(media);
+            mediaPlayer.play();
+
+            mediaPlayer.setOnEndOfMedia(() -> {
+                mediaPlayer.dispose();
+                tempFile.delete();
+                requestNextSongFromServer();
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeConnection() {
+        if (socket != null && !socket.isClosed()) {
+            try {
+                socket.close();
+                System.out.println("Connection closed");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- mrezni-plejer/src/application/SNovi.java
+++ mrezni-plejer/src/application/SNovi.java
@@ -1,11 +1,19 @@
-package application; 
+package application;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class SNovi {
     private static final int SERVER_PORT = 6666;
     private static final String MUSIC_FOLDER = "C:/Users/libor/OneDrive/Desktop/zika/";
+
+    private static boolean isSendingMusic = false;
+    private static Queue<String> songQueue = new LinkedList<>();
+    private static List<Socket> connectedClients = new LinkedList<>();
 
     public static void main(String[] args) {
         try {
@@ -16,34 +24,51 @@ public class SNovi {
                 Socket socket = serverSocket.accept();
                 System.out.println("Client connected");
 
-                new Thread(() -> {
+                synchronized (connectedClients) {
+                    connectedClients.add(socket);
+                }
+
+                Thread clientThread = new Thread(() -> {
                     try {
-                        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-                        String songName = dataInputStream.readUTF();
-
-                        File musicFolder = new File("C:/Users/libor/OneDrive/Desktop/zika/");
-                        File songFile = findSongFile(musicFolder, songName);
-
-                        if (songFile != null) {
-                            // Send the song data to the client
-                            sendSongToClient(socket, songFile);
-                        } else {
-                            // Song not found, send error message to the client
-                            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-                            dataOutputStream.writeUTF("Song not found: " + songName);
-                            dataOutputStream.flush();
-                        }
-
-                        socket.close();
-                        System.out.println("Client disconnected");
+                        handleClient(socket);
                     } catch (IOException e) {
                         e.printStackTrace();
+                    } finally {
+                        synchronized (connectedClients) {
+                            connectedClients.remove(socket);
+                        }
                     }
-                }).start();
+                });
+                clientThread.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void handleClient(Socket socket) throws IOException {
+        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+        String songName = dataInputStream.readUTF();
+
+        File musicFolder = new File("C:/Users/libor/OneDrive/Desktop/zika/");
+        File songFile = findSongFile(musicFolder, songName);
+
+        if (songFile != null) {
+            synchronized (songQueue) {
+                songQueue.add(songName);
+                if (!isSendingMusic) {
+                    isSendingMusic = true;
+                    sendMusicToClients();
+                }
+            }
+        } else {
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            dataOutputStream.writeUTF("Song not found: " + songName);
+            dataOutputStream.flush();
+        }
+
+        socket.close();
+        System.out.println("Client disconnected");
     }
 
     private static File findSongFile(File folder, String songName) {
@@ -58,28 +83,53 @@ public class SNovi {
         return null;
     }
 
-    private static void sendSongToClient(Socket socket, File songFile) throws IOException {
+    private static void sendMusicToClients() {
+        while (true) {
+            String songName;
+            synchronized (songQueue) {
+                if (songQueue.isEmpty()) {
+                    isSendingMusic = false;
+                    break;
+                }
+                songName = songQueue.poll();
+            }
+
+            try {
+                File musicFolder = new File("C:/Users/libor/OneDrive/Desktop/zika/");
+                File songFile = findSongFile(musicFolder, songName);
+
+                if (songFile != null) {
+                    broadcastSong(songFile);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void broadcastSong(File songFile) throws IOException {
         byte[] buffer = new byte[8192];
         FileInputStream fileInputStream = new FileInputStream(songFile);
         BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
-        OutputStream outputStream = socket.getOutputStream();
 
-        // Send the size of the song data to the client
-        long songSize = songFile.length();
-        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-        dataOutputStream.writeLong(songSize);
+        synchronized (connectedClients) {
+            for (Socket clientSocket : connectedClients) {
+                OutputStream outputStream = clientSocket.getOutputStream();
 
-        // Send the song data to the client
-        int bytesRead;
-        while ((bytesRead = bufferedInputStream.read(buffer)) != -1) {
-            outputStream.write(buffer, 0, bytesRead);
+                long songSize = songFile.length();
+                DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+                dataOutputStream.writeLong(songSize);
+
+                int bytesRead;
+                while ((bytesRead = bufferedInputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+
+                outputStream.flush();
+            }
         }
 
-        outputStream.flush();
         bufferedInputStream.close();
         fileInputStream.close();
     }
-
-
 }
-
